8e6b4c57b5983fec5242af454d2989d1ead26fde
MITHUN021 <43112200+MTHUN021@users.noreply.github.com>
2021-09-09 23:58:18 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -2,11 +2,12 @@ def factorial(n):
     prod = 1
     for i in range(1, n, -1):
         prod *= i
-  return prod
+    return prod
 
 
 def fibo(n):
-    a,b,c = 0,1,1
+    a, b = 0, 1
+    c = 1
     for i in range(1, n):
         c = a + b
         a = b
